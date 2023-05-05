76160e8a83cd2e3a26e490905437360701b42c5e
Gabuni19 <124473199+Gabuni19@users.noreply.github.com>
2023-05-04 20:56:53 -0500

    Create Flotante.py
    Esta función usa normalizar por eso es recomendable definir la función normalizar antes

---
+++ Analisis y Modelamiento Numérico I/Flotante.py
@@ -0,0 +1,36 @@
+def normalizar(x):
+    """Funcion que nomarliza un valor x ala forma : 0.[...]e[...]
+    
+        Parametros posicionales:
+        x -- numero a normalizar
+    """
+    #Creamos un variable que indique si es positivo
+    positivo=1    
+    #Si x es un numero negativo pasarlo a positivo
+    if x<0:
+        x=abs(x)
+        positivo=-1;
+    
+    if x==0: 
+        return (0,0);
+    elif (x>1):
+        t=len(str(int(x)))
+        x=x/10**t
+        return (positivo*x,10**t)
+    else:
+        t=len(str(x).split(".")[-1])        
+        aux=x*10**t        
+        m=len(str(int(aux)))
+        x=x*10**(t-m)
+        return (positivo*x,10**-(t-m))
+      
+def fl(x,decimales):
+    """Funcion que calcula el fl(x) de un numero x
+    
+        Parametros posicionales:
+        x -- numero real
+        decimales -- decimales a redondear
+    """
+    (d,potencia)=normalizar(x)
+    d = round(d,decimales)
+    return (d,potencia)
